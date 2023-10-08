c7d3a42fb7c750444670eb4f8624365dec98a44a
Rasmus Bo Jørgensen <daerjo@egmont.com>
2023-10-08 18:40:37 +0200

    Initial example

---
+++ README.md
@@ -0,0 +1,23 @@
+# Go API Example
+How to create a REST api with gin and swagger in go.
+
+## Swagger
+To generate documentation run `swag init` which will generate and update the contents of the `docs` directory.
+
+``` shell
+swag init --parseDependency
+```
+
+Then start the application.
+
+``` shell
+go run main.go
+```
+
+And open the swagger site at [http://localhost:8080/swagger/index.html](http://localhost:8080/swagger/index.html).
+
+## Links
+Links for resources used in this example.
+
+- https://github.com/swaggo/swag/blob/master/example/basic/main.go
+- https://github.com/swaggo/swag
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/swaggo/swag/example/celler/controller"
+	_ "github.com/swaggo/swag/example/celler/docs"
+	"github.com/swaggo/swag/example/celler/httputil"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func main() {
+	r := gin.Default()
+
+	c := controller.NewController()
+
+	v1 := r.Group("/api/v1")
+	{
+		accounts := v1.Group("/accounts")
+		{
+			accounts.GET(":id", c.ShowAccount)
+			accounts.GET("", c.ListAccounts)
+			accounts.POST("", c.AddAccount)
+			accounts.DELETE(":id", c.DeleteAccount)
+			accounts.PATCH(":id", c.UpdateAccount)
+			accounts.POST(":id/images", c.UploadAccountImage)
+		}
+		bottles := v1.Group("/bottles")
+		{
+			bottles.GET(":id", c.ShowBottle)
+			bottles.GET("", c.ListBottles)
+		}
+		admin := v1.Group("/admin")
+		{
+			admin.Use(auth())
+			admin.POST("/auth", c.Auth)
+		}
+		examples := v1.Group("/examples")
+		{
+			examples.GET("ping", c.PingExample)
+			examples.GET("calc", c.CalcExample)
+			examples.GET("groups/:group_id/accounts/:account_id", c.PathParamsExample)
+			examples.GET("header", c.HeaderExample)
+			examples.GET("securities", c.SecuritiesExample)
+			examples.GET("attribute", c.AttributeExample)
+		}
+	}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.Run(":8080")
+}
+
+func auth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if len(c.GetHeader("Authorization")) == 0 {
+			httputil.NewError(c, http.StatusUnauthorized, errors.New("Authorization is required Header"))
+			c.Abort()
+		}
+		c.Next()
+	}
+}
